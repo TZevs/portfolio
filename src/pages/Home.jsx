4e83f8eb033a57e81cf96a77b71aa8a8da8146ee
Thalia Evans <148969711+TZevs@users.noreply.github.com>
2025-05-28 21:32:33 +0100

    updating home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,28 +7,52 @@ import ParticlesBg from "../components/Particles";
 
 export default function Home() {
   return (
-    <Container fluid className="home-container">
-      <ParticlesBg />
-      <Container className="home-content">
-        <Row>
-          <Col className="home-header">
-            <h1 className="heading">Hi There 👋</h1>
-            <h2 className="heading-name">
-              I'M <span className="main-colour">THALIA EVANS</span>
-            </h2>
-            <h3 className="main-colour" style={{ padding: 50 }}>
-              Software Engineering Student
-            </h3>
-          </Col>
-          <Col className="home-header-img">
-            <img
-              src={ProfileImg}
-              alt="Profile Picture"
-              className="header-img"
-            />
-          </Col>
-        </Row>
+    <>
+      <Container fluid className="home-container">
+        <Container className="home-content">
+          <Row>
+            <Col className="home-header">
+              <h1 className="heading">Hi There 👋</h1>
+              <h2 className="heading-name">
+                I'M <span className="main-colour">THALIA EVANS</span>
+              </h2>
+              <h3 className="main-colour" style={{ padding: 50 }}>
+                Software Engineering Student
+              </h3>
+            </Col>
+            <Col className="home-header-img">
+              <img
+                src={ProfileImg}
+                alt="Profile Picture"
+                className="header-img"
+              />
+            </Col>
+          </Row>
+        </Container>
       </Container>
-    </Container>
+      <Container fluid className="home-about-container">
+        <Container className="home-content">
+          <Row>
+            <Col sm={7} className="home-header">
+              <h1 className="about-heading">
+                LET ME <span className="main-colour">INTRODUCE</span> MYSELF
+              </h1>
+              <div className="home-about-section">
+                <p className="home-about">
+                  I am passionate about software developing and programming,
+                  despite how challenging it can get.
+                </p>
+                <p>I am confident in languages like C# and JavaScript.</p>
+                <p>
+                  My current interests are in developing Web applications using
+                  different Frameworks.
+                </p>
+              </div>
+            </Col>
+            <Col className="home-bottom-img"></Col>
+          </Row>
+        </Container>
+      </Container>
+    </>
   );
 }
