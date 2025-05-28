391063cd69cae9f2cd78d480d288db9902c5a0d8
Thalia Evans <148969711+TZevs@users.noreply.github.com>
2025-05-28 11:29:12 +0100

    renamed css file import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,47 @@
-import "./Navbar.css";
-import { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { AiOutlineHome, AiOutlineUser, AiOutlineFundProjectionScreen } from "react-icons/ai";
+import "./navbar.css";
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineFundProjectionScreen,
+} from "react-icons/ai";
 
 export default function CustomNavbar() {
   const [expand, setExpand] = useState(false);
-  const location = useLocation(); 
+  const location = useLocation();
   const [navColour, updateNavbar] = useState(false);
-  
+
   useEffect(() => {
-  const handleScroll = () => {
-    if (window.scrollY >= 20) {
-      updateNavbar(true);
-    } else {
-      updateNavbar(false);
-    }
-  };
+    const handleScroll = () => {
+      if (window.scrollY >= 20) {
+        updateNavbar(true);
+      } else {
+        updateNavbar(false);
+      }
+    };
 
-  window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-  return () => {
-    window.removeEventListener("scroll", handleScroll);
-  };
-}, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <Navbar expanded={expand} expand="md" fixed="top" className={navColour ? "sticky" : "navbar"}>
+    <Navbar
+      expanded={expand}
+      expand="md"
+      fixed="top"
+      className={navColour ? "sticky" : "navbar"}
+    >
       <Container>
-        <Navbar.Brand as={Link} to="/" className="logo">TzE</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="logo">
+          TzE
+        </Navbar.Brand>
         <Navbar.Toggle
           className="custom-toggle"
           aria-controls="responsive-navbar-nav"
@@ -46,25 +57,32 @@ export default function CustomNavbar() {
               as={Link}
               to="/"
               onClick={() => setExpand(false)}
-              className={`custom-link ${location.pathname === '/' ? 'active' : ''}`}
+              className={`custom-link ${
+                location.pathname === "/" ? "active" : ""
+              }`}
             >
-              <AiOutlineHome style={{marginBottom: 3.5}}/> Home
+              <AiOutlineHome style={{ marginBottom: 3.5 }} /> Home
             </Nav.Link>
             <Nav.Link
               as={Link}
               to="/about"
               onClick={() => setExpand(false)}
-              className={`custom-link ${location.pathname === '/about' ? 'active' : ''}`}
+              className={`custom-link ${
+                location.pathname === "/about" ? "active" : ""
+              }`}
             >
-              <AiOutlineUser style={{marginBottom: 3.5}} /> About
+              <AiOutlineUser style={{ marginBottom: 3.5 }} /> About
             </Nav.Link>
             <Nav.Link
               as={Link}
               to="/projects"
               onClick={() => setExpand(false)}
-              className={`custom-link ${location.pathname === '/projects' ? 'active' : ''}`}
+              className={`custom-link ${
+                location.pathname === "/projects" ? "active" : ""
+              }`}
             >
-              <AiOutlineFundProjectionScreen style={{marginBottom: 3.5}} /> Projects
+              <AiOutlineFundProjectionScreen style={{ marginBottom: 3.5 }} />{" "}
+              Projects
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
