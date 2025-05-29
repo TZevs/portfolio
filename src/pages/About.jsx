90347fffc4f8accedf7beb3ddf2a73d6ea522c8e
Thalia Evans <148969711+TZevs@users.noreply.github.com>
2025-05-29 14:04:58 +0100

    top section of about page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,59 @@
+import "./about.css";
+import Container from "react-bootstrap/Container";
+import { FiArrowRightCircle } from "react-icons/fi";
+
 export default function About() {
-    return (
-        <h1>About Me</h1>
-    )
+  return (
+    <>
+      <Container className="about-container">
+        <Container className="about-content">
+          <h1>
+            Who I <span className="main-colour">Am</span>
+          </h1>
+          <p>
+            Hello Everyone, I am{" "}
+            <span className="main-colour">Thalia Evans</span> from{" "}
+            <span className="main-colour">Sheffield, England.</span>
+          </p>
+          <p>
+            I have just finished my 2nd year as a{" "}
+            <span className="main-colour">software engineering student</span> at
+            Sheffield Hallam University.
+          </p>
+          <p>
+            I completed an{" "}
+            <span className="main-colour">Extended National Diploma in IT</span>{" "}
+            at The Sheffield College, with a triple distinction (DDD).
+          </p>
+          <p>
+            My <span className="main-colour">hobbies</span> outside of
+            programming:
+          </p>
+          <ul style={{ paddingLeft: 0 }}>
+            <li className="about-hobby">
+              <FiArrowRightCircle
+                className="main-colour"
+                style={{ marginBottom: 3.5, marginRight: 3 }}
+              />
+              Art - Sketching, 3D models, Painting
+            </li>
+            <li className="about-hobby">
+              <FiArrowRightCircle
+                className="main-colour"
+                style={{ marginBottom: 3.5, marginRight: 3 }}
+              />
+              Reading - Primarily Fantasy
+            </li>
+            <li className="about-hobby">
+              <FiArrowRightCircle
+                className="main-colour"
+                style={{ marginBottom: 3.5, marginRight: 3 }}
+              />
+              Sport - Football, Swimming, Running
+            </li>
+          </ul>
+        </Container>
+      </Container>
+    </>
+  );
 }
