98612c7da6efc7e1af55e426a2b1a5b849df4877
Thalia Evans <148969711+TZevs@users.noreply.github.com>
2025-05-30 16:58:36 +0100

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import ScrollToTop from "./components/ScrollToTop";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import "./footer.css";
+import { Container, Row, Col } from "react-bootstrap";
+import { AiOutlineGithub } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+
+export default function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-col">
+          <h6 style={{ marginBottom: 0 }}>Created by Thalia Evans</h6>
+        </Col>
+        <Col md="4" className="footer-col">
+          <h6
+            style={{ textAlign: "center", marginBottom: 0, fontWeight: "300" }}
+          >
+            Copyright © {year}
+          </h6>
+        </Col>
+        <Col md="4" className="footer-col">
+          <ul className="footer-icon-list">
+            <li className="footer-icon-item">
+              <a
+                href="https://github.com/TZevs"
+                target="_blank"
+                className="footer-icons "
+              >
+                <AiOutlineGithub />
+              </a>
+            </li>
+            <li className="footer-icon-item">
+              <a
+                href="https://www.linkedin.com/in/thalia-evans-935516314/"
+                target="_blank"
+                className="footer-icons "
+              >
+                <FaLinkedin />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
