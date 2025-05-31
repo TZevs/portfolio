3568593fd5e24d9297cf99a802f887898278de73
Thalia Evans <148969711+TZevs@users.noreply.github.com>
2025-05-31 12:16:44 +0100

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/portfolio/",
-  build: {
-    outDir: "docs", // instead of dist
-  },
   plugins: [react()],
 })
 
